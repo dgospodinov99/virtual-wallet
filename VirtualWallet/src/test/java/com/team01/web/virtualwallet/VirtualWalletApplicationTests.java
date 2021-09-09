76e1b22e99a5793e02455c35a12595fa39d47cd6
Dian Gospodinov <dian,gospodinov99@gmail.com>
2021-09-09 19:12:41 +0300

    Initial project commit

---
+++ VirtualWallet/src/test/java/com/team01/web/virtualwallet/VirtualWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team01.web.virtualwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
