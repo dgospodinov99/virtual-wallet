d178ca8306109ba82a74c6963a4ae4e0a769f8a6
Bojidar <bojidar.velichkov.a30@learn.telerikacademy.com>
2021-09-10 13:59:51 +0300

    Initial commit of User classes with Dto

--- VirtualWallet/src/main/java/com/team01/web/virtualwallet/repositories/UserRepositoryImpl.java
+++ VirtualWallet/src/main/java/com/team01/web/virtualwallet/repositories/UserRepositoryImpl.java
@@ -1,4 +1,92 @@
 package com.team01.web.virtualwallet.repositories;
 
-public class UserRepositoryImpl {
+import com.team01.web.virtualwallet.exceptions.EntityNotFoundException;
+import com.team01.web.virtualwallet.models.User;
+import com.team01.web.virtualwallet.repositories.contracts.UserRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<User> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = session.createQuery("from User order by id", User.class);
+            return query.list();
+        }
+    }
+
+    @Override
+    public User getById(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            User user = session.get(User.class, id);
+            if (user == null) {
+                throw new EntityNotFoundException("User", id);
+            }
+            return user;
+        }
+    }
+
+    @Override
+    public User getByUsername(String username) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = session.createQuery("from User where username = :username", User.class);
+            query.setParameter("username", username);
+            if (query.list().size() == 0) {
+                throw new EntityNotFoundException("User", "username", username);
+            }
+            return query.list().get(0);
+        }
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = session.createQuery("from User where email = :email", User.class);
+            query.setParameter("email", email);
+            if (query.list().size() == 0) {
+                throw new EntityNotFoundException("User", "email", email);
+            }
+            return query.list().get(0);
+        }
+    }
+
+    @Override
+    public void create(User user) {
+        try (Session session = sessionFactory.openSession()) {
+            session.save(user);
+        }
+    }
+
+    @Override
+    public void update(User user) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.update(user);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            User user = getById(id);
+            session.delete(user);
+            session.getTransaction().commit();
+        }
+    }
 }
