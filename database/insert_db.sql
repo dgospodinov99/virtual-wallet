214d794a0b200d9da839bf04df86390b01e0bf7f
Dian Gospodinov <dian,gospodinov99@gmail.com>
2021-09-11 13:56:01 +0300

    Update db insert

--- database/insert_db.sql
+++ database/insert_db.sql
@@ -2,13 +2,19 @@ insert into `virtual-wallet`.roles (role_id, name)
 values  (1, 'User'),
         (2, 'Administrator');
 
-
-insert into `virtual-wallet`.users (user_id, username, email, phone_number, password, photo_url, balance)
-values  (1, 'guygilbert123', 'GuyGilbert@gmail.com', '00000000', 'Parola123@', '',0.00),
-        (2, 'kevinbrown123', 'KevinBrown@gmail.com', '00000001', 'Parola123@', '',0.00),
-        (3, 'roberto123', 'RobertoTamburello@gmail.com', '00000002', 'Parola123@', '',0.00),
-        (4, 'dian.g123', 'DianGospodinov@gmail.com', '00000003', 'Parola123@', '',1000000.00),
-        (5, 'bojidar.v123', 'BojidarVelichkov@gmail.com', '00000004', 'Parola123@', '',1000000.00);
+insert into `virtual-wallet`.wallets(wallet_id, balance)
+values (1,0),
+       (2,0),
+       (3,0),
+       (4,10000),
+       (5,10000);
+       
+insert into `virtual-wallet`.users (user_id, username, email, phone_number, password, photo_url, wallet_id)
+values  (1, 'guygilbert123', 'GuyGilbert@gmail.com', '00000000', 'Parola123@', '',1),
+        (2, 'kevinbrown123', 'KevinBrown@gmail.com', '00000001', 'Parola123@', '',2),
+        (3, 'roberto123', 'RobertoTamburello@gmail.com', '00000002', 'Parola123@', '',3),
+        (4, 'dian.g123', 'DianGospodinov@gmail.com', '00000003', 'Parola123@', '',4),
+        (5, 'bojidar.v123', 'BojidarVelichkov@gmail.com', '00000004', 'Parola123@', '',5);
 
 insert into `virtual-wallet`.cards (card_id, card_number, holder, check_number, user_id)
 values  (1, '1111111111111111', 'Guy Gilbert', '000', 1),
@@ -17,10 +23,6 @@ values  (1, '1111111111111111', 'Guy Gilbert', '000', 1),
         (4, '1111111111111114', 'Dian Gospodinov', '000', 4),
         (5, '1111111111111115', 'Bojidar Velichkov', '000', 5);
 
-
-insert into `virtual-wallet`.transfers (transfer_id,sender_id, receiver_id, amount)
-values  (1,4,5,100.00),
-        (2,5,4,100.00);
 
 insert into `virtual-wallet`.users_cards(user_id, card_id)
 values  (1,1),
