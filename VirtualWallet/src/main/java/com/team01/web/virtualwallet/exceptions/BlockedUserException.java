bb2a4503493b98525726db9889c9d524c5c05847
Dian Gospodinov <dian,gospodinov99@gmail.com>
2021-09-12 15:50:06 +0300

    Implement BlockedUserException

---
+++ VirtualWallet/src/main/java/com/team01/web/virtualwallet/exceptions/BlockedUserException.java
@@ -0,0 +1,8 @@
+package com.team01.web.virtualwallet.exceptions;
+
+public class BlockedUserException extends RuntimeException{
+
+    public BlockedUserException(String message) {
+        super(String.format("%s", message));
+    }
+}
