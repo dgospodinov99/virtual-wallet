fb3a162665cd679979a5d78b88509454f03ac6ef
Dian Gospodinov <dian,gospodinov99@gmail.com>
2021-09-12 16:33:28 +0300

    Implement UnauthorizedOperationException

---
+++ VirtualWallet/src/main/java/com/team01/web/virtualwallet/exceptions/UnauthorizedOperationException.java
@@ -0,0 +1,8 @@
+package com.team01.web.virtualwallet.exceptions;
+
+public class UnauthorizedOperationException extends RuntimeException{
+
+    public UnauthorizedOperationException(String message) {
+        super(message);
+    }
+}
