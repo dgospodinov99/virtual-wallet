bab07e46a413a9499d58c9ebf6d276a6ac849582
Dian Gospodinov <dian,gospodinov99@gmail.com>
2021-09-12 16:34:36 +0300

    Implement BlockUserDto

---
+++ VirtualWallet/src/main/java/com/team01/web/virtualwallet/models/dto/BlockUserDto.java
@@ -0,0 +1,14 @@
+package com.team01.web.virtualwallet.models.dto;
+
+public class BlockUserDto {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
