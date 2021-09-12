0305ec8b0463f9e3f6806ecd8c3cf1bbdf8d395c
Bojidar <bojidar.velichkov.a30@learn.telerikacademy.com>
2021-09-12 13:52:10 +0300

    Create GlobalExceptionHandler for Rest Controllers

---
+++ VirtualWallet/src/main/java/com/team01/web/virtualwallet/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.team01.web.virtualwallet.controllers;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value
+            = { IllegalArgumentException.class, IllegalStateException.class })
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "The body has invalid values";
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
