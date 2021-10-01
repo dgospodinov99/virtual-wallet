ffea3f7e0fd98fec25aa1778ead8efe02d25f3f9
Bojidar <bojidar.velichkov.a30@learn.telerikacademy.com>
2021-10-01 14:34:21 +0300

    Add random token generation and send link through email

--- VirtualWallet/src/main/java/com/team01/web/virtualwallet/services/EmailServiceImpl.java
+++ VirtualWallet/src/main/java/com/team01/web/virtualwallet/services/EmailServiceImpl.java
@@ -1,22 +1,34 @@
 package com.team01.web.virtualwallet.services;
 
 import com.team01.web.virtualwallet.config.EmailConfig;
+import com.team01.web.virtualwallet.models.Token;
 import com.team01.web.virtualwallet.services.contracts.EmailService;
+import com.team01.web.virtualwallet.services.contracts.TokenService;
+import com.team01.web.virtualwallet.services.contracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
+import java.util.Random;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
     private final EmailConfig emailConfig;
     private final JavaMailSender mailSender;
+    private final TokenService tokenService;
+    private final UserService userService;
 
     @Autowired
-    public EmailServiceImpl(EmailConfig emailConfig, JavaMailSender mailSender) {
+    public EmailServiceImpl(EmailConfig emailConfig, JavaMailSender mailSender, TokenService tokenService, UserService userService) {
         this.emailConfig = emailConfig;
         this.mailSender = mailSender;
+        this.tokenService = tokenService;
+        this.userService = userService;
     }
 
     @Override
@@ -31,9 +43,34 @@ public class EmailServiceImpl implements EmailService {
     }
 
     @Override
-    public void sendVerifyRegistrationEmail(String recipientEmail, int code) {
-        String message = "Your verification code is: " + code;
-        sendSimpleMessage(recipientEmail, "Registration Verification", message);
+    public Token sendVerifyRegistrationEmail(String recipientEmail) {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String token = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        SimpleMailMessage toSend = new SimpleMailMessage();
+        String url="http://localhost:8080/auth/verify/" + token;
+        String content="<a href='"+url+"'>"+url+"</a>";
+        String message = "Please click on the following link to complete your registration: \n" + content;
+        MimeMessagePreparator preparator = mimeMessage -> {
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipientEmail));
+            mimeMessage.setFrom(new InternetAddress(emailConfig.getUsername()));
+            mimeMessage.setSubject("Email Verification");
+            mimeMessage.setText(message, "UTF-8", "html");
+        };
+        Token tokenToSave = new Token();
+        tokenToSave.setActive(true);
+        tokenToSave.setUser(userService.getByEmail(recipientEmail));
+        tokenToSave.setToken(token);
+        tokenService.create(tokenToSave);
+        mailSender.send(preparator);
+        return tokenToSave;
     }
 
     @Override
--- VirtualWallet/src/main/java/com/team01/web/virtualwallet/services/contracts/EmailService.java
+++ VirtualWallet/src/main/java/com/team01/web/virtualwallet/services/contracts/EmailService.java
@@ -1,12 +1,13 @@
 package com.team01.web.virtualwallet.services.contracts;
 
+import com.team01.web.virtualwallet.models.Token;
 import org.springframework.mail.SimpleMailMessage;
 
 public interface EmailService {
 
     SimpleMailMessage sendSimpleMessage(String to, String subject, String text);
 
-    void sendVerifyRegistrationEmail(String recipientEmail, int code);
+    Token sendVerifyRegistrationEmail(String recipientEmail);
 
     void sendVerifyTransactionEmail(String recipientEmail, int code);
 
