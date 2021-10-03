fd9c9aac727a0d08d471c9dccb144c4e7dbb8587
Dian Gospodinov <dian,gospodinov99@gmail.com>
2021-10-03 12:06:46 +0300

    Add error message to CustomErrorController

--- VirtualWallet/src/main/java/com/team01/web/virtualwallet/controllers/MVC/CustomErrorController.java
+++ VirtualWallet/src/main/java/com/team01/web/virtualwallet/controllers/MVC/CustomErrorController.java
@@ -3,6 +3,8 @@ package com.team01.web.virtualwallet.controllers.MVC;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,17 +21,23 @@ public class CustomErrorController implements ErrorController {
     }
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request,
+                              Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
+            model.addAttribute("error", request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
 
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error404";
             } else if (statusCode == HttpStatus.UNAUTHORIZED.value()) {
                 return "error401";
-            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            }  else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
+                return "error400";
+            }    else if (statusCode == HttpStatus.CONFLICT.value()) {
+                return "error409";
+            }   else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
                 return "error500";
             }
 
