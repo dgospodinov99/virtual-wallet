57fe8e99bc5aa306f206947c2be4f7c05c239d52
Bojidar <bojidar.velichkov.a30@learn.telerikacademy.com>
2021-10-03 17:39:59 +0300

    Add InvalidTokenException

---
+++ VirtualWallet/src/main/java/com/team01/web/virtualwallet/exceptions/InvalidTokenException.java
@@ -0,0 +1,9 @@
+package com.team01.web.virtualwallet.exceptions;
+
+public class InvalidTokenException extends RuntimeException{
+
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+
+}
