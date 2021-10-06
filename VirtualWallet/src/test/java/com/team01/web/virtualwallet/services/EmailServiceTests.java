8d198986b2633bff2130b6da6d2fff5621d047e8
Dian Gospodinov <dian,gospodinov99@gmail.com>
2021-10-06 19:01:43 +0300

    Remove typo file

--- VirtualWallet/src/test/java/com/team01/web/virtualwallet/services/EmailServiceTests.java
+++
@@ -1,9 +0,0 @@
-package com.team01.web.virtualwallet.services;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public class EmailServiceTests {
-
-}
