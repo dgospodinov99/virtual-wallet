0daf2726ea33b9b1aa79a416735f367facb41d0b
Dian Gospodinov <dian,gospodinov99@gmail.com>
2021-10-06 19:01:03 +0300

    Update db insert file

---
+++ VirtualWallet/src/test/java/com/team01/web/virtualwallet/services/EmailServiceTests.java
@@ -0,0 +1,9 @@
+package com.team01.web.virtualwallet.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailServiceTests {
+
+}
